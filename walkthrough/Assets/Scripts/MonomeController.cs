01a71c36f78c4001ab7b40618cb57c3c778a91ab
bloomingbridges <brueckner.florian@googlemail.com>
2012-11-23 00:27:30 +0000

    A fully working monome :3

--- walkthrough/Assets/Scripts/MonomeController.cs
+++ walkthrough/Assets/Scripts/MonomeController.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using LitJson;
 
 public class MonomeController : MonoBehaviour {
 	
@@ -9,9 +12,18 @@ public class MonomeController : MonoBehaviour {
 	public  int prev         = 8;
 	private int amountRows   = 9;
 	private int amountPerRow = 9;
+
+	public string OSCHost   = "127.0.0.1";
+	public int SendToPort   = 1338;
+	public int ListenerPort = 8888;
 	
-	private Color32 baseColour  = new Color32(100,100,100,255);
-	private Color32 activeColour = new Color32(129,208,224,255);
+	private UDPPacketIO udp;
+	private Osc handler;
+	
+	private Stack<int> idBuffer = new Stack<int>();
+	private Stack<int> stateBuffer = new Stack<int>();
+	
+	private bool sync = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,15 +36,84 @@ public class MonomeController : MonoBehaviour {
 				clone.transform.parent = gameObject.transform;
 				clone.SendMessage("SetID", (row * amountPerRow) + (i+1));
 				clone.tag = "col" + (i+1);
-				clone.renderer.material.color = baseColour;
 			}
 			row++;
 		}
+		
+		if(udp = (UDPPacketIO) gameObject.GetComponent("UDPPacketIO")) {
+			udp.init(OSCHost, SendToPort, ListenerPort);
+			        
+			handler = (Osc) gameObject.GetComponent("Osc");
+			handler.init(udp);
+			handler.SetAddressHandler("/press", IncomingValues);
+			handler.SetAddressHandler("/sync", ResetTime);
+		}
+		
+		OSCHandler.Instance.Init();
+		
 		startTime = Time.time;
 	}
+				
+	void SendToSaul (string s) {
+		OSCHandler.Instance.SendMessageToClient("Saul", "/press", s);
+	}
+	
+	void IncomingValues(OscMessage msg) {
+		// Debug.Log("Values! " + msg.Values[0]);
+		JsonData change = JsonMapper.ToObject((string) msg.Values[0]);
+		// Debug.Log("Change in " + change["location"]["name"]);
+		int colour = -1;
+		switch ((string) change["location"]["name"]) {
+			/*
+			case "blazey":
+				colour = 1;
+				break;
+			*/
+			case "austell":
+				colour = 2;
+				break;
+			case "fowey":
+				colour = 3;
+				break;
+		}
+		if ((int) change["key"]["state"] == 0)
+			colour = 0;
+		UpdateSquare((int) change["key"]["id"], colour);
+	}
+	
+	void UpdateSquare(int id, int state) {
+		idBuffer.Push(id);
+		stateBuffer.Push(state);
+	}
+	
+	void ResetTime(OscMessage msg) {
+		sync = true;
+	} 
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if (sync) {
+			startTime = Time.time;
+			timePassed = 0f;
+			sync = false;
+			Debug.Log("SYNCHED UP");
+		}
+		
+		while (idBuffer.Count > 0){
+			
+			int id = idBuffer.Pop();
+			int state = stateBuffer.Pop();
+			int row = (int) Mathf.Floor(id / 9);
+			int col = id % amountPerRow;
+			// Debug.Log("In row " + row + ", column " + col + "?");
+			
+			GameObject[] squares = GameObject.FindGameObjectsWithTag("col"+(col+1));
+			squares[row].SendMessage("SetState", state);
+			// Debug.Log(squares[row]);
+			// Debug.Log(idBuffer.Count + " changes in buffer.");
+		}
+		
 		timePassed += Time.deltaTime;
 		if (timePassed >= 0.250f) {
 			
@@ -56,6 +137,10 @@ public class MonomeController : MonoBehaviour {
 			}
 			
 		}
+		
+		 if (Input.GetKeyDown(KeyCode.H))
+            SendToSaul("HELLO SAUL!");
 
 	}
+	
 }
--- walkthrough/Assets/Scripts/MonomeSquareBehaviour.cs
+++ walkthrough/Assets/Scripts/MonomeSquareBehaviour.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections;
+using LitJson;
+using System.Text;
 
 public class MonomeSquareBehaviour : MonoBehaviour {
 	
@@ -7,13 +9,15 @@ public class MonomeSquareBehaviour : MonoBehaviour {
 	public int state = 0;
 	private Color32[] colours = new Color32[4];
 	private Color32 highlightColour = new Color32(200,200,200,255);
+	private GameObject monome;
 	
 	// Use this for initialization
 	void Start () {
 		colours[0] = new Color32(100,100,100,255);
 		colours[1] = new Color32(129,208,224,255);
-		colours[2] = new Color32(129,208,224,255);
-		colours[3] = new Color32(129,208,224,255);
+		colours[2] = new Color32(244,83,85,255);
+		colours[3] = new Color32(102,198,140,255);
+		monome = GameObject.Find("Monome");
 	}
 	
 	// Update is called once per frame
@@ -36,18 +40,47 @@ public class MonomeSquareBehaviour : MonoBehaviour {
 		id = myID;
 	}
 	
+	void SetState (int col) {
+		if (col > -1) {
+			renderer.material.color = colours[col];
+			state = col;
+		}
+	}
+	
 	void Activate (int s) {
 		renderer.material.color = colours[s];
 		state = s;
+		
+		StringBuilder sb = new StringBuilder();
+        JsonWriter writer = new JsonWriter(sb);
+		writer.WriteObjectStart();
+        writer.WritePropertyName("id");
+        writer.Write(id);
+		writer.WritePropertyName("state");
+		writer.Write(1);
+        writer.WriteObjectEnd();
+		
+		monome.SendMessage("SendToSaul", sb.ToString());
 	}
 	
 	void Deactivate () {
 		state = 0;
 		renderer.material.color = colours[0];
+		
+		StringBuilder sb = new StringBuilder();
+        JsonWriter writer = new JsonWriter(sb);
+		writer.WriteObjectStart();
+        writer.WritePropertyName("id");
+        writer.Write(id);
+		writer.WritePropertyName("state");
+		writer.Write(0);
+        writer.WriteObjectEnd();
+		
+		monome.SendMessage("SendToSaul", sb.ToString());
 	}
 	
 	void OnTriggerEnter (Collider col) {
-		Debug.Log ("Standing on #" + id);
+		// Debug.Log ("Standing on #" + id);
 		Activate(1);
 	}
 	
